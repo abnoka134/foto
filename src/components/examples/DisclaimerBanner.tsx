8d645e1e5b1ca02702d74c27f86f2cb2dd214873
abnoka134 <vtt0103@gmail.com>
2026-03-16 15:23:50 +0700

    Delete src directory

--- src/components/examples/DisclaimerBanner.tsx
+++
@@ -1,5 +0,0 @@
-import DisclaimerBanner from "../DisclaimerBanner";
-
-export default function DisclaimerBannerExample() {
-  return <DisclaimerBanner />;
-}
--- src/components/examples/ProcessingProgress.tsx
+++
@@ -1,7 +0,0 @@
-import ProcessingProgress from "../ProcessingProgress";
-
-export default function ProcessingProgressExample() {
-  return (
-    <ProcessingProgress progress={65} isVisible={true} current={2} total={3} />
-  );
-}
