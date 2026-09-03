1fe7c518099439dc06178249a2fb9c9fadb0ca47
abnoka134 <vtt0103@gmail.com>
2026-09-03 10:05:07 +0700

    Update watermark.ts

--- client/src/lib/watermark.ts
+++ client/src/lib/watermark.ts
@@ -283,7 +283,7 @@ export async function applyWatermark(
   ctx.fillStyle = "white";
   ctx.fillText(watermarkData.time, timeX, timeY);
   ctx.strokeStyle = "white"; 
-  ctx.lineWidth = 0.8 * scaleFactor;
+  ctx.lineWidth = 1 * scaleFactor;
   ctx.strokeText(watermarkData.time, timeX, timeY);
   ctx.shadowColor = "transparent";
 
@@ -363,7 +363,7 @@ export async function applyWatermark(
 
   ctx.fillStyle = "white";
   ctx.strokeStyle = "white";
-  ctx.lineWidth = 0.15 * scaleFactor;
+  ctx.lineWidth = 0.2 * scaleFactor;
 
   let currentX: number = brandStartX;
   for (const char of subText) {
