a385794fdb842239838664762514058582ad4889
abnoka134 <vtt0103@gmail.com>
2026-09-03 10:38:39 +0700

    Update watermark.ts

--- client/src/lib/watermark.ts
+++ client/src/lib/watermark.ts
@@ -376,7 +376,7 @@ export async function applyWatermark(
   ctx.shadowColor = "transparent";
 
   ctx.strokeStyle = "white";
-  ctx.lineWidth = 0.8 * scaleFactor; 
+  ctx.lineWidth = 0.9 * scaleFactor; 
 
   currentX = brandStartX;
   for (const char of subText) {
